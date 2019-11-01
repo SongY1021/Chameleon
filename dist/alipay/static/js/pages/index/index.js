af6a6e6826cd3f9437e7824aeb185b72a015a968
SongY1021 <syang_010@163.com>
2019-11-01 18:31:59 +0800

    Chameleon项目测试

---
+++ dist/alipay/static/js/pages/index/index.js
@@ -0,0 +1,92 @@
+var __CML__GLOBAL = require("../../manifest.js");
+__CML__GLOBAL.webpackJsonp([9],{
+
+/***/ "./C:/Users/sy/AppData/Roaming/npm/node_modules/chameleon-tool/node_modules/babel-loader/lib/index.js?{\"filename\":\"C:\\\\Users\\\\sy\\\\AppData\\\\Roaming\\\\npm\\\\node_modules\\\\chameleon-tool\\\\chameleon.js\"}!./C:/Users/sy/AppData/Roaming/npm/node_modules/chameleon-tool/node_modules/chameleon-loader/src/selector.js?type=script&index=0&fileType=page&media=dev&cmlType=alipay&isInjectBaseStyle=true&check={\"enable\":true,\"enableTypes\":[]}!./src/pages/index/index.cml":
+/***/ (function(module, exports, __webpack_require__) {
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _chameleonRuntime = __webpack_require__("./node_modules/chameleon-runtime/index.js");
+
+var _chameleonRuntime2 = _interopRequireDefault(_chameleonRuntime);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Scroller = function () {
+    function Scroller() {
+        _classCallCheck(this, Scroller);
+
+        this.data = {
+            /**
+             * scroller 配置
+             */
+            bottomOffset: 20,
+            scrollDirection: 'vertical',
+            panels: [],
+            rows: []
+        };
+        this.methods = {
+            randomfn: function randomfn() {
+                var ary = [];
+                for (var i = 1; i <= 40; i++) {
+                    var item = { label: i, height: 200, width: 730, bgc: '#69BE96', opacity: 1 };
+                    item.computedStyle = 'height:' + item.height + 'cpx;width:' + item.width + 'cpx;background-color:' + item.bgc + ';opacity:' + item.opacity;
+
+                    ary.push(item);
+                }
+                return ary;
+            },
+            onScroll: function onScroll(e) {
+                console.log(e);
+            },
+            onBottom: function onBottom(e) {
+                console.log(e);
+            }
+        };
+    }
+
+    _createClass(Scroller, [{
+        key: 'created',
+        value: function created(res) {
+            this.panels = this.randomfn();
+
+            for (var i = 0; i < 30; i++) {
+                this.rows.push('row ' + i);
+            }
+            console.log('demo page created:', res);
+        }
+    }]);
+
+    return Scroller;
+}();
+
+exports.default = new Scroller();
+
+
+exports.default = _chameleonRuntime2.default.createPage(exports.default).getOptions();
+
+/***/ }),
+
+/***/ "./C:/Users/sy/AppData/Roaming/npm/node_modules/chameleon-tool/node_modules/cml-extract-css-webpack-plugin/dist/loader.js?{\"omit\":1,\"remove\":true}!./C:/Users/sy/AppData/Roaming/npm/node_modules/chameleon-tool/node_modules/vue-style-loader/index.js!./C:/Users/sy/AppData/Roaming/npm/node_modules/chameleon-tool/node_modules/css-loader/index.js?{\"sourceMap\":false}!./C:/Users/sy/AppData/Roaming/npm/node_modules/chameleon-tool/node_modules/chameleon-css-loader/index.js?{\"platform\":\"miniapp\",\"cmlType\":\"alipay\"}!./C:/Users/sy/AppData/Roaming/npm/node_modules/chameleon-tool/node_modules/postcss-loader/lib/index.js?{\"sourceMap\":false,\"config\":{\"path\":\"C:\\\\Users\\\\sy\\\\AppData\\\\Roaming\\\\npm\\\\node_modules\\\\chameleon-tool\\\\configs\\\\postcss\\\\alipay\\\\.postcssrc.js\"}}!./C:/Users/sy/AppData/Roaming/npm/node_modules/chameleon-tool/node_modules/less-loader/dist/cjs.js?{\"sourceMap\":false}!./C:/Users/sy/AppData/Roaming/npm/node_modules/chameleon-tool/node_modules/chameleon-css-loader/index.js?{\"media\":true,\"cmlType\":\"alipay\"}!./C:/Users/sy/AppData/Roaming/npm/node_modules/chameleon-tool/node_modules/chameleon-loader/src/selector.js?type=styles&index=0&fileType=page&media=dev&cmlType=alipay&isInjectBaseStyle=true&check={\"enable\":true,\"enableTypes\":[]}!./src/pages/index/index.cml":
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ "./src/pages/index/index.cml":
+/***/ (function(module, exports, __webpack_require__) {
+
+var __cml__style0 = __webpack_require__("./C:/Users/sy/AppData/Roaming/npm/node_modules/chameleon-tool/node_modules/cml-extract-css-webpack-plugin/dist/loader.js?{\"omit\":1,\"remove\":true}!./C:/Users/sy/AppData/Roaming/npm/node_modules/chameleon-tool/node_modules/vue-style-loader/index.js!./C:/Users/sy/AppData/Roaming/npm/node_modules/chameleon-tool/node_modules/css-loader/index.js?{\"sourceMap\":false}!./C:/Users/sy/AppData/Roaming/npm/node_modules/chameleon-tool/node_modules/chameleon-css-loader/index.js?{\"platform\":\"miniapp\",\"cmlType\":\"alipay\"}!./C:/Users/sy/AppData/Roaming/npm/node_modules/chameleon-tool/node_modules/postcss-loader/lib/index.js?{\"sourceMap\":false,\"config\":{\"path\":\"C:\\\\Users\\\\sy\\\\AppData\\\\Roaming\\\\npm\\\\node_modules\\\\chameleon-tool\\\\configs\\\\postcss\\\\alipay\\\\.postcssrc.js\"}}!./C:/Users/sy/AppData/Roaming/npm/node_modules/chameleon-tool/node_modules/less-loader/dist/cjs.js?{\"sourceMap\":false}!./C:/Users/sy/AppData/Roaming/npm/node_modules/chameleon-tool/node_modules/chameleon-css-loader/index.js?{\"media\":true,\"cmlType\":\"alipay\"}!./C:/Users/sy/AppData/Roaming/npm/node_modules/chameleon-tool/node_modules/chameleon-loader/src/selector.js?type=styles&index=0&fileType=page&media=dev&cmlType=alipay&isInjectBaseStyle=true&check={\"enable\":true,\"enableTypes\":[]}!./src/pages/index/index.cml");
+var __cml__script = __webpack_require__("./C:/Users/sy/AppData/Roaming/npm/node_modules/chameleon-tool/node_modules/babel-loader/lib/index.js?{\"filename\":\"C:\\\\Users\\\\sy\\\\AppData\\\\Roaming\\\\npm\\\\node_modules\\\\chameleon-tool\\\\chameleon.js\"}!./C:/Users/sy/AppData/Roaming/npm/node_modules/chameleon-tool/node_modules/chameleon-loader/src/selector.js?type=script&index=0&fileType=page&media=dev&cmlType=alipay&isInjectBaseStyle=true&check={\"enable\":true,\"enableTypes\":[]}!./src/pages/index/index.cml");
+
+
+/***/ })
+
+},["./src/pages/index/index.cml"]);
---
+++ dist/baidu/pages/index/index.swan
@@ -0,0 +1,39 @@
+<view class="container  cml-base cml-view">
+    <cml-buildin-row height="{{80}}" class=" cml-base cml-row">
+        <cml-buildin-col width="{{240}}" height="{{80}}" background-color="#ddd" class=" cml-base cml-col">
+        <view class=" cml-base cml-view"></view>
+        </cml-buildin-col>
+        <cml-buildin-col width="{{510}}" height="{{80}}" background-color="#bbb" class=" cml-base cml-col">
+        <view class=" cml-base cml-view"></view>
+        </cml-buildin-col>
+    </cml-buildin-row>
+    <cml-buildin-scroller scroll-direction="{{scrollDirection}}" bottom-offset="{{bottomOffset}}" bindscrolltobottom="_cmlEventProxy" data-eventscrolltobottom="{{['onBottom']}}" bindonscroll="_cmlEventProxy" data-eventonscroll="{{['onScroll']}}" height="{{-1}}" class=" cml-base cml-scroller">
+        <view class=" cml-base cml-view">
+        <swiper class="container  cml-base cml-carousel" indicator-dots="{{true}}" current="{{1}}" circular="{{true}}">
+            <swiper-item class=" cml-base cml-carousel-item">
+                <view class="carousel-item  cml-base cml-view" style="background-color:#EBDEAA"></view>
+            </swiper-item>
+            <swiper-item class=" cml-base cml-carousel-item">
+                <view class="carousel-item  cml-base cml-view" style="background-color:#E3EDCD"></view>
+            </swiper-item>
+            <swiper-item class=" cml-base cml-carousel-item">
+                <view class="carousel-item  cml-base cml-view" style="background-color:#EAEAEF"></view>
+            </swiper-item>
+        </swiper>
+        </view>
+        <view class="advert  cml-base cml-view">
+            <text class="text  cml-base cml-text"></text>
+        </view>
+        
+                
+                
+                
+                
+                
+        
+            
+                
+            
+        
+    </cml-buildin-scroller>
+</view>
